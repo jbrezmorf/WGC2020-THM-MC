38034d003108849a27c074920143a3a1995adfe8
Jan Brezina <jan.brezina@tul.cz>
2019-07-14 10:01:16 +0200

    Avg temp and power plot

--- fixed-3-frac/process.py
+++ fixed-3-frac/process.py
@@ -2,6 +2,7 @@ import subprocess
 import yaml
 import attr
 import numpy as np
+import matplotlib.pyplot as plt
 
 @attr.s(auto_attribs=True)
 class ValueDesctription:
@@ -72,17 +73,71 @@ def get_result_description():
     return power_series + temp_series
 
 
+def extract_time_series(yaml_stream, regions, extract):
+    """
+
+    :param yaml_stream:
+    :param regions:
+    :return: times list, list: for every region the array of value series
+    """
+    data = yaml.safe_load(yaml_stream)['data']
+    times = set()
+    reg_series = {reg: [] for reg in regions}
+
+    for time_data in data:
+        region = time_data['region']
+        if region in reg_series:
+            times.add(time_data['time'])
+            power_in_time = extract(time_data)
+            reg_series[region].append(power_in_time)
+    times = list(times)
+    times.sort()
+    series = [np.array(region_series) for region_series in reg_series.values()]
+    return np.array(times), series
+
 def extract_results(config_dict):
     """
     :param config_dict: Parsed config.yaml. see key comments there.
     : return
     """
-    pass
+    abs_zero_temp = 273.15
+    year_sec = 60 * 60 * 24 *365
+    bc_regions = ['.left_fr_left_well', '.left_well', '.right_fr_right_well', '.right_well']
+    out_regions = bc_regions[2:]
+    with open("output_th/energy_balance.yaml", "r") as f:
+        power_times, reg_powers = extract_time_series(f, bc_regions, extract=lambda frame: frame['data'][0])
+    power_series = -sum(reg_powers)
+
+    with open("output_th/Heat_AdvectionDiffusion_region_stat.yaml", "r") as f:
+        temp_times, reg_temps = extract_time_series(f, out_regions, extract=lambda frame: frame['average'][0])
+    with open("output_th/water_balance.yaml", "r") as f:
+        flux_times, reg_fluxes = extract_time_series(f, out_regions, extract=lambda frame: frame['data'][0])
+    sum_flux = sum(reg_fluxes)
+    avg_temp = sum([temp * flux for temp, flux in zip(reg_temps, reg_fluxes)]) / sum_flux
+
+    fig, ax1 = plt.subplots()
+    temp_color = 'red'
+    ax1.set_xlabel('time [y]')
+    ax1.set_ylabel('Temperature [C deg]', color=temp_color)
+    ax1.plot(temp_times[1:] / year_sec, avg_temp[1:] - abs_zero_temp, color=temp_color)
+    ax1.tick_params(axis='y', labelcolor=temp_color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    pow_color = 'blue'
+    ax2.set_ylabel('Power [MW]', color=pow_color)  # we already handled the x-label with ax1
+    ax2.plot(power_times[1:] / year_sec, power_series[1:] / 1e6, color=pow_color)
+    ax2.tick_params(axis='y', labelcolor=pow_color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
     with open("config.yaml", "r") as f:
         config_dict = yaml.safe_load(f)
     #compute_hm(config_dict)
     #prepare_th_input(config_dict)
-    compute_th(config_dict)
+    #compute_th(config_dict)
     extract_results(config_dict)
