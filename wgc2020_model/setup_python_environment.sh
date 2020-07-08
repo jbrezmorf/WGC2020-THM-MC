49cbed128fffe9b1cb8897914d67a983d1e1955e
Paulie14 <pavelexner@gmail.com>
2020-07-08 10:53:02 +0200

    Add env and modules setup scripts for Charon.

---
+++ wgc2020_model/setup_python_environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Make sure you called 'source load_modules.sh' before!"
+echo "Creating python environment.."
+python3 -m venv env
+
+# source ./load_modules.sh
+source env/bin/activate
+python --version
+which python
+which pip
+pip install --upgrade pip
+pip -V
+
+pip install pyyaml attrs numpy
+pip install -e ../bgem
+pip install -e ../MLMC
+
+#pip freeze
+deactivate
+
